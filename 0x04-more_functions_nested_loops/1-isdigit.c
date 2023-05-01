86d258a57f8b08866853dbbd608b02abe41ee1d1
rebeccandiwa <niwarebecca44@gmail.com>
2023-04-30 23:00:46 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
-/** 
+/**
  * _isdigit - Check if a character is a digit
  * @c: The number to be checked
  * Return: 1 for a character that will be a digit 0 for any else
-*/
+ */
 
 int _isdigit(int c)
 {
+
 	if (c >= 48 && c <= 57)
 	{
 	return (1);
